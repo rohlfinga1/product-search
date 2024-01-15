dff25789e4c5e6edee5ef118f53217c00f9b5a99
Amy Drapac <rohlfinga1@gmail.com>
2024-01-15 12:05:28 -0500

    1-15-24 pair program read product in file, take user input

--- ProductSearch/Program.cs
+++ ProductSearch/Program.cs
@@ -10,20 +10,95 @@ Console.WriteLine("Hello, World!");
 // does user want to add ipad? no - back to search
 
 // open file
-    // get file path
-    // get file name
-    // combine path
-    // open path
+// get file path
+// get file name
+// combine path
+// open path
+var fileName = "input-file.json";
+var path = Path.GetDirectoryName(fileName);
+var combinedPath = Path.Combine(path, fileName);
 
-// open streamreader
+var read = new StreamReader(combinedPath);
+
+Console.WriteLine($"What is the product name?");
+var userInput = Console.ReadLine();
+var product = new Product();
+
+if (userInput != null)
+{
+    try
+    {
+        while (!read.EndOfStream)
+        {
+            var line = read.ReadLine();
+
+            if (line != null && line.Contains("name") && (line.ToLower()).Contains(userInput.ToLower()))
+            {
+                var value = line.Substring(9);
+                product.Name = value.Substring(0, value.Length - 2).Trim();
+            }
+            else if (line != null && line.Contains("price"))
+            {
+                var value = line.Substring(9);
+                var final = Decimal.Parse(value.Substring(0, value.Length - 1).Trim());
+                product.Price = final;
+            }
+            else if (line != null && line.Contains("quantity"))
+            {
+                var value = int.Parse(line.Substring(12));
+                product.Quantity = value;
+            }
+
+            if (product.Name != null && product.Price != null && product.Quantity != null)
+            {
+                if (product.Name.ToLower() == userInput.ToLower())
+                {
+                    Console.WriteLine($"Name: {product.Name}");
+                    Console.WriteLine($"Price: {product.Price}");
+                    Console.WriteLine($"Quantity on hand: {product.Quantity}");
+                }
+            }
+        }
+    }
+    catch
+    {
+
+    }
+}
+
+string answer;
+
+if (product == null)
+{
+    Console.WriteLine($"Sorry, that product was not found in our inventory.");
+    Console.WriteLine($"Would you like to add this product to the inventory? Y/N ");
+    answer = Console.ReadLine();
+}
+else
+{
+    answer = "";
+}
+
+if (answer.ToLower() != "y")
+{
+
+}
+else
+{
+
+}
+
 // ask user for input
+// open streamreader
+
 // Ensure that the product search is case insensitive.
 // • When a product is not found, ask if the product should be added.
 //  If yes, ask for the price and the quantity, and save it in the JSON file.
-//  Ensure the newly added product is immediately available for searching withoutrestarting the program
+//  Ensure the newly added product is immediately available for searching without restarting the program
+//  (write without exiting)
 // clean user input
 // read from file
-// if an item we read has a name that mataches our user input, then we grab json object contianing name.
+// if an item we read has a name that mataches our user input, then we grab json object containing name.
 // error handle
 // output
 // close streamreader
